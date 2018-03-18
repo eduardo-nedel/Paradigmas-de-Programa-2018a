d9f8d0fc1840bf597b6aac399370f9f47d438f30
eduardo-nedel <emnedel@inf.ufsm.br>
2018-03-18 19:02:06 -0300

    Função de Número 11 resolvida
    Peço perdão por estar dando 2 commits seguidos, não havia visto que precisava enviar o que havia feito em aula e o resto indo por commits...No próximo trabalho prestarei mais atenção às descrições.

--- t1/t1.hs
+++ t1/t1.hs
@@ -81,7 +81,18 @@ encodeName :: String -> String
 encodeName str = map valorVogal str
 
 -- 11. Escreva uma função betterEncodeName :: String -> String que substitua vogais em uma string, conforme este esquema: a = 4, e = 3, i = 1, o = 0, u = 00.
+valorVogal2 :: Char -> String
+valorVogal2 c
+  | (toLower c) == 'a' = "4"
+  | (toLower c) == 'e' = "3"
+  | (toLower c) == 'i' = "1"
+  | (toLower c) == 'o' = "0"
+  | (toLower c) == 'u' = "00"
+  | otherwise = [c]
 
+
+betterEncodeName :: String -> String
+betterEncodeName str = concatMap (\x -> valorVogal2 x) str
 
 -- 12. Dada uma lista de strings, produzir outra lista com strings de 10 caracteres, usando o seguinte esquema: strings de entrada com mais de 10 caracteres são truncadas, strings com até 10 caracteres são completadas com '.' até ficarem com 10 caracteres.
 tenCharacters :: [String] -> [String]
