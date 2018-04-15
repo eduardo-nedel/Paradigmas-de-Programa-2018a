48681a518ee4e8104b5c6e2c757b6717086ede87
eduardo-nedel <emnedel@inf.ufsm.br>
2018-04-15 14:54:47 -0300

    Atividades 4 e 5 concluídas

--- t3/t3.pl
+++ t3/t3.pl
@@ -9,3 +9,30 @@ has5(L) :- L = [_,_,_,_,_].
 % 3. Defina um predicado hasN(L,N) que é verdadeiro se L for uma lista de N elementos.
 
 hasN(L, N) :- length(L, A), A =:= N.
+
+% 4. Defina um predicado potN0(N,L), de forma que L seja uma lista de potências de 2, com expoentes de N a 0.
+
+potN0(-1, []).
+potN0(N, L) :-
+    L = [H|T],
+    X is N-1,
+    H is 2**N,
+    potN0(X, T).
+
+/* 5. Defina um predicado zipmult(L1,L2,L3), de forma que cada elemento da lista L3 seja o produto dos elementos 
+de L1 e L2 na mesma posição do elemento de L3. */
+
+zipmult([], _, []).
+zipmult(L1, L2, L3) :-
+    L1 = [H1|T1],
+    L2 = [H2|T2],
+    L3 = [H3|T3],
+    H3 is H1*H2,
+    zipmult(T1, T2, T3).
+
+/* 6. Defina um predicado potencias(N,L), de forma que L seja uma lista com as N primeiras potências de 2, 
+sendo a primeira 2^0 e assim por diante.. */
+
+potencias(N,L) :- 
+    
+
