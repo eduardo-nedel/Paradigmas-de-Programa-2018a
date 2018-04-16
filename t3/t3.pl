2c7814fa0f3a7f27c1de16d38946b471feef57d9
eduardo-nedel <emnedel@inf.ufsm.br>
2018-04-16 10:51:47 -0300

    Atividade 7 e 8 Concluídas

--- t3/t3.pl
+++ t3/t3.pl
@@ -38,7 +38,27 @@ potencias(N,L) :-
     potN0(N-1, AUX),
     reverse(AUX, L).
 
+% 7. Defina um predicado positivos(L1,L2), de forma que L2 seja uma lista só com os elementos positivos de L1.
 
+positivos([], []).
+positivos(L1, L2) :-
+    L1 = [H1|T1],
+    H1 > 0,
+    L2 = [H2|T2],
+    H2 is H1,
+    positivos(T1, T2).
 
+positivos(L1, L2) :-
+    L1 = [_|T1],
+    positivos(T1, L2).
+
+/* 8. Considere que L1 e L2 sejam permutações de uma lista de elementos distintos, sem repetições. Sabendo disso, 
+defina um predicado mesmaPosicao(A,L1,L2) para verificar se um elemento A está na mesma posição nas listas L1 e L2. */
+
+mesmaPosicao(A, [A|_], [A|_]).
+mesmaPosicao(A,L1,L2) :-
+    L1 = [_|T1],
+    L2 = [_|T2],
+    mesmaPosicao(A, T1, T2).
     
 
