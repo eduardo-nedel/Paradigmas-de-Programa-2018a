c0e8bd62aff8823ea3f9bc10325738a319a64363
eduardo-nedel <emnedel@inf.ufsm.br>
2018-05-16 16:34:13 -0300

    Extra: Exercício:Minecraft
    Alterado

--- extras/mini mine/Mini_mine.java
+++ extras/mini mine/Mini_mine.java
@@ -1,40 +1,61 @@
 interface Bloco{
-    public void quebra(String ferramenta);
+    public Bloco quebra(String ferramenta);
+
+    public String toString();
 }
 
 
 class Madeira implements Bloco{
-    public void quebra(String ferramenta){
-        System.out.println("Madeira foi dropada");
+    public Bloco quebra(String ferramenta){
+        return this;
     }
 
+    public String toString(){
+        return "= Madeira";
+    }
 }
 
 class Grama implements Bloco{
-    public void quebra(String ferramenta){
-        System.out.println("Terra foi dropada");
+    public Bloco quebra(String ferramenta){
+        return new Terra();
+    }
+
+    public String toString(){
+        return "= Grama";
     }
 
 }
 
 class Terra implements Bloco{
-    public void quebra(String ferramenta){
-        System.out.println("Terra foi dropada");
+    public Bloco quebra(String ferramenta){
+        return this;
     }
 
+    public String toString(){
+        return "= Terra";
+    }
 }
 
 class Pedra implements Bloco{
-    public void quebra(String ferramenta){
-        if (ferramenta == "picareta") {
-            System.out.println("Pedregulho foi dropado");
-        }
+    public Bloco quebra(String ferramenta){
+        if (ferramenta == "picareta")
+            return new Pedregulho();
+        else
+            return null;
+    }
+
+    public String toString(){
+        return "= Pedra";
     }
 }
 
 class Pedregulho implements Bloco{
-    public void quebra(String ferramenta){
-        System.out.println("Pedregulho foi dropado");
+    public Bloco quebra(String ferramenta){
+        return this;
+    }
+
+    public String toString(){
+        return "= Pedregulho";
     }
 
 }
@@ -43,11 +64,29 @@ class Pedregulho implements Bloco{
 
 class Mini_mine{
     public static void main(String[] args) {
-        Bloco blocos[] = { new Madeira(), new Grama(), new Terra(), new Pedra(), new Pedregulho()};
+        Bloco b1 =  new Madeira(), b2 = new Grama(), b3 = new Terra(), b4 = new Pedra(), b5 = new Pedregulho(), b6 = new Pedra();
 
-        for (Bloco b: blocos) {
-            b.quebra("picareta");
-        }
+        System.out.println("Bloco a serem quebrados com nada " + b1);
+        System.out.println("Bloco a serem quebrados com nada " + b2);
+        System.out.println("Bloco a serem quebrados com nada " + b3);
+        System.out.println("Bloco a serem quebrados com picareta " + b4);
+        System.out.println("Bloco a serem quebrados com nada " + b5);
+        System.out.println("Bloco a serem quebrados com nada " + b6);
+
+        b1 = b1.quebra("");
+        b2 = b2.quebra("");
+        b3 = b3.quebra("");
+        b4 = b4.quebra("picareta");
+        b5 = b5.quebra("");
+        b6 = b6.quebra("");
+
+        System.out.println("Objeto resultante " + b1);
+        System.out.println("Objeto resultante " + b2);
+        System.out.println("Objeto resultante " + b3);
+        System.out.println("Objeto resultante " + b4);
+        System.out.println("Objeto resultante " + b5);
+        System.out.println("Objeto resultante " + b6);
+
     }
 
 }
